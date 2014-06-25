9970954c5db884f5210e12c72edb0c6f899768b6
Daryll Santos <daryll.santos@gmail.com>
2014-06-25 15:54:01 +0800

    Update expenses spec to pass.

--- spec/requests/api/v1/expenses_spec.rb
+++ spec/requests/api/v1/expenses_spec.rb
@@ -8,5 +8,18 @@ describe 'Expenses API' do
       json = JSON.parse(response.body)
       expect(json).to eq []
     end
+
+    it 'returns all expenses' do
+      expense1 = create(:expense, name: 'Foodies')
+      expense2 = create(:expense, name: 'Hoodies')
+
+      get '/api/v1/expenses', {}, { "Accept" => "application/json" }
+
+      json = JSON.parse(response.body)
+
+      test_names = json.map { |x| x['name'] }
+
+      expect(test_names).to eq ['Foodies', 'Hoodies']
+    end
   end
 end
