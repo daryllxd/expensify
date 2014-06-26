ca98460e97e3479607b291416dc0e832c604f062
Daryll Santos <daryll.santos@gmail.com>
2014-06-26 18:32:00 +0800

    Add url to Expense serializer.

--- app/serializers/expense_serializer.rb
+++ app/serializers/expense_serializer.rb
@@ -1,3 +1,7 @@
 class ExpenseSerializer < ActiveModel::Serializer
-  attributes :id, :name, :description, :category
+  attributes :id, :name, :description, :category, :url
+
+  def url
+    api_v1_expense_path(object)
+  end
 end
--- spec/serializers/expense_serializer_spec.rb
+++ spec/serializers/expense_serializer_spec.rb
@@ -2,13 +2,13 @@ require 'spec_helper'
 
 describe ExpenseSerializer do
   it 'creates JSON for the API' do
-    serializer = ExpenseSerializer.new(Expense.new(
+    serializer = ExpenseSerializer.new(Expense.create(
       id: 1,
       name: 'Foodies',
       description: 'Eating some food.',
       category: 'Food'
     ))
-    expect(serializer.to_json).to eql('{"expense":{"id":1,"name":"Foodies","description":"Eating some food.","category":"Food"}}')
 
+    expect(serializer.to_json).to eql('{"expense":{"id":1,"name":"Foodies","description":"Eating some food.","category":"Food","url":"/api/v1/expenses/1"}}')
   end
 end
