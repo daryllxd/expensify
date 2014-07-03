f0a428f10af8ff98593a463623bc5b71259e40a9
Daryll Santos <daryll.santos@gmail.com>
2014-07-03 14:06:59 +0800

    Remove Font-awesome from Bower and use own gem. Add ngRoute package for Angular.

--- app/assets/javascripts/expensify-angular.js
+++ app/assets/javascripts/expensify-angular.js
@@ -1,7 +1,8 @@
 //= require angular
 //= require angular-resource
+//= require angular-route
 //= require_self
 //= require_tree ./angular/initializers
 //= require_tree ./angular
 
-Expensify = angular.module('Expensify', ["ngResource"]);
+Expensify = angular.module('Expensify', ["ngResource", "ngRoute"]);
